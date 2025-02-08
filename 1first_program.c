adeec8900dc783f78c217d758b2e51d2aece5e3d
WingCatcher <catcherwing@gmail.com>
2025-02-08 20:36:53 +0700

    new file:   1first_program.c 	new file:   2helloworld.c 	new file:   3comments_escapesequences.c 	new file:   4variables.c

---
+++ 1first_program.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    printf("I like pizza\nIts really good\nDo you like pizaa too?");
+    printf("\nHey its written in c now\nDont you miss print() era?\n");
+    printf("Its been a long journey now, wish you a real developer");
+    return 0;
+}
---
+++ 3comments_escapesequences.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+    printf("today we are learning comments");
+    printf("I like pizza\nIts really good\nDo you like pizaa too?");
+    printf("\nHey its written in c now\nDont you miss print() era?\n");
+    printf("\n\nName\tRaffy Ahmad\nKelas\tX8");
+    printf("Do not give up - \"Leonardo\"");
+
+    // Single line comments
+    /*
+    This is
+    Multi line
+    Comment
+    */
+
+    /*
+    escape sequences = character combination consisting of a backslash \ followed
+    by a letter or combination of digits.
+    They specify actions within a line of string of text.
+    \n = newline
+    \t = tab
+    \"test\"= "test" quotes
+
+    escape sequences = kombinasi karakter yang terdiri dari garis miring \ diikuti
+    dengan huruf atau kombinasi angka.
+    Karakter ini menentukan tindakan dalam satu baris string teks.
+    \n = baris baru
+    \t = tab
+    */
+}
